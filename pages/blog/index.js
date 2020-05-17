33bc6a9076188bfef743e3f2ac79fa45d79e976e
sentoneibisu <sazaesanwa.kimo.kunai@gmail.com>
2020-05-17 10:29:44 +0900

    second commit

---
+++ pages/blog/index.js
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+export default (req, res) => {
+    return (
+        <Head>
+            <title>ブログ投稿作成</title>
+            <main>
+                <h1 className="title">
+                    ブログ投稿作成ページ
+                </h1>
+            </main>
+        </Head>
+    )
+}
---
+++ pages/instagram/index.js
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+export default (req, res) => {
+    return (
+      <Head>
+        <title>Instagram 投稿作成</title>
+        <main>
+          <h1 className="title">
+            Instagram 投稿作成ページ
+          </h1>
+        </main>
+      </Head>
+    )
+}
